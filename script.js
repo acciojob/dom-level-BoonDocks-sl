b251fc2534daedebb7e8cd34e2a85f7628da96a1
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-03 10:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  var element = document.getElementById("level");
+  var level = 0;
+
+  while (element.parentElement && element.parentElement.tagName === "UL") {
+    element = element.parentElement;
+    level++;
+  }
+
+  alert("The level of the element is: " + level);
+});
